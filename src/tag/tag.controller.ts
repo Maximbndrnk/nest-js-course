8209a307b558cb7bb2a2bf2bed87f91a1989c4e1
Maxim Bondarenko <maximbndrnk@gmail.com>
2024-08-29 11:13:54 +0300

    add base tag controller

---
+++ src/tag/tag.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { TagService } from './tag.service';
+
+@Controller('tags')
+export class TagController {
+    constructor(
+        private tagService: TagService
+    ) {
+    }
+
+    @Get()
+    findAll(): string[] {
+        return this.tagService.findAll();
+    }
+
+}
---
+++ src/tag/tag.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TagService {
+    constructor() {
+    }
+
+
+    findAll(): string[]{
+        return ['dragons', 'coffee','weather'];
+    }
+}
