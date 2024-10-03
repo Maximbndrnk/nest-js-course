16f6653d7f22ab062324a46b59e79276f61938c6
Maxim Bondarenko <maximbndrnk@gmail.com>
2024-10-03 10:57:46 +0300

    add swagger :)

--- src/user/dto/createUser.dto.ts
+++ src/user/dto/createUser.dto.ts
@@ -1,13 +1,17 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto{
+    @ApiProperty()
     @IsNotEmpty()
     readonly username: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsEmail()
     readonly email: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     readonly password: string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,12 +3,11 @@ import { UserService } from '@app/user/user.service';
 import { CreateUserDto } from '@app/user/dto/createUser.dto';
 import { UserResponseInterface } from '@app/user/types/user-response.model';
 import { LoginUserDto } from '@app/user/dto/loginUser.dto';
-import { ExpressRequestInterface } from '@app/types/expressRequest.interface';
 import { UserDecorator } from '@app/user/decorators/user.decorator';
 import { UserEntity } from '@app/user/user.entity';
 import { AuthGuard } from '@app/user/guards/auth.guard';
 import { UpdateUserDto } from '@app/user/dto/updateUser.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('User')
 @Controller()
@@ -19,6 +18,7 @@ export class UserController {
     ) {
     }
 
+    @ApiBody({ type: CreateUserDto })
     @Post('users')
     @UsePipes(new ValidationPipe())
     async createUser(@Body('user') createUserDTO: CreateUserDto): Promise<UserResponseInterface> {
